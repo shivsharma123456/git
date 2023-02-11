0affe186c389cecb7a463b69575e651341a945d1
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-02-11 12:19:01 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -25,4 +25,72 @@ thirdItem.style.color = 'coral';
 // odd[i].style.backgroundColor = '#f4f4f4'; 
 // even[i].style.backgroundColor = '#ccc';
 
-// }
+// TRAVERSING THE DOM
+var itemList = document.querySelector('#items'); 
+// parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4'; 
+console.log(itemList.parentNode.parentNode.parentNode);
+// parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4'
+console.log(itemList.parentElement.parentElement.parentElement);
+// childNodes
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
+// Firstchild
+console.log(itemList.firstChild);
+// fristElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
+
+// lastchild
+console.log(itemList.lastChild);
+// lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+console.log(itemList.nextSibling);
+
+// nextElementSibling
+console.log(itemList.nextElementSibling);
+
+// previouSibling
+console.log(itemList.previousSibling);
+// previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = 'green';
+// createElement
+// Create a div
+
+var newDiv= document.createElement('div');
+
+// Add class
+
+newDiv.className= 'hello';
+
+// Add id
+
+newDiv.id = 'hello1';
+
+// Add attr
+
+newDiv.setAttribute('title', 'Hello Div');
+
+console.log(newDiv);
+// Create text node
+
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container'); 
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
